1d8491525db1195a1dae394f630c67ca84be87e6
Michael Aufreiter <ma@zive.at>
2012-03-15 15:06:26 -0400

    Birthday.

---
+++ proxy.js
@@ -0,0 +1,12 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var options = {
+  router: {
+    'letterpress.substance.io': '127.0.0.1:4004',
+    'substance.io': '127.0.0.1.com:3003'
+  }
+};
+
+var proxyServer = httpProxy.createServer(options);
+proxyServer.listen(80);
