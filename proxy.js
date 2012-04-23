193010b3b935b9d78b9d87a0a92d5c93c3802ea2
Michael Aufreiter <ma@zive.at>
2012-04-22 22:57:59 -0400

    Added ot.substance.io

--- proxy.js
+++ proxy.js
@@ -5,7 +5,8 @@ var options = {
   router: {
     'letterpress.substance.io': '127.0.0.1:4004',
     'substance.io': '127.0.0.1:3003',
-    'composer.substance.io': 127.0.0.1:5005
+    'composer.substance.io': '127.0.0.1:5005',
+    'ot.substance.io': '127.0.0.1:5000'
   }
 };
 
