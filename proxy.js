a7b5115b533ea5e345d4b264eb6ddfea9a43fd67
Michael Aufreiter <ma@zive.at>
2012-05-03 16:08:39 -0400

    Added Dejavis.org

--- proxy.js
+++ proxy.js
@@ -6,6 +6,8 @@ var options = {
     'letterpress.substance.io': '127.0.0.1:4004',
     'substance.io': '127.0.0.1:3003',
     'composer.substance.io': '127.0.0.1:5005',
+    'dejavis.org': '127.0.0.1:6006',
+    'sandbox.dejavis.org': '127.0.0.1:6007',
     'ot.substance.io': '127.0.0.1:5000'
   }
 };
